597f7fc87458f77b654690bc7f5f9e205f3997fa
Roy Wallace <waldo000000@gmail.com>
2018-03-08 09:59:11 +1000

    Concatenate multiple files per channel (like TRMs) and playback seamlessly

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,108 @@
 const audioCtx = new AudioContext()
-const rangeInput = document.getElementById('range') as HTMLInputElement
 
-const audios: Audio[] = [
-  {
-    name: 'Besound',
-    path: 'bensound-energy.aac'
-  },
-  {
-    name: 'Going higher',
-    path: 'bensound-goinghigher.aac'
-  }
-]
-  
-interface Audio {
-  name: string
+const recording: Recording = {
+  numberOfChannels: 2,
+  trms: [
+    {
+      channels: [
+        { path: 'audio/dummy_channel_1_000.aac' },
+        { path: 'audio/dummy_channel_2_000.aac' }
+      ]
+    },
+    {
+      channels: [
+        { path: 'audio/dummy_channel_1_001.aac' },
+        { path: 'audio/dummy_channel_2_001.aac' }
+      ]
+    },
+    {
+      channels: [
+        { path: 'audio/dummy_channel_1_002.aac' },
+        { path: 'audio/dummy_channel_2_002.aac' }
+      ]
+    }
+  ]
+}
+
+interface Recording {
+  numberOfChannels: number
+  trms: Trm[]
+}
+
+interface Trm {
+  // Assumes all channels have same length and sample rate
+  channels: ChannelFile[]
+}
+
+interface ChannelFile {
   path: string
 }
 
-function getAudioData(path): Promise<AudioBuffer> {
-  return new Promise<AudioBuffer> (resolve => {
+interface LoadedBuffer {
+  buffer: AudioBuffer
+  path: string
+}
+function getAudioBuffer (path): Promise<LoadedBuffer> {
+  return new Promise<LoadedBuffer>(resolve => {
     const request = new XMLHttpRequest()
-
     request.open('GET', path, true)
     request.responseType = 'arraybuffer'
 
-    request.onload = function() {
+    request.onload = function () {
       const audioData = request.response
 
-      audioCtx.decodeAudioData(audioData, function(buffer) {
-          resolve(buffer)
-        })
+      audioCtx.decodeAudioData(audioData, function (buffer) {
+        const loadedBuffer: LoadedBuffer = {
+          buffer,
+          path
+        }
+        resolve(loadedBuffer)
+      })
     }
 
     request.send()
   })
 }
 
-async function addAudio(audio: Audio): Promise<AudioBufferSourceNode> {
-  const data = await getAudioData(audio.path)
+// Note: Assumes each input buffer has only 1 channel
+function concatenateBuffers (buffers: LoadedBuffer[]) {
 
-  const src = audioCtx.createBufferSource()
-  src.buffer = data
+  if (buffers.some(b => b.buffer.numberOfChannels > 1)) {
+    throw new RangeError(`Unexpected number of channels in channel data`)
+  }
+
+  const totalBufferLength = buffers.reduce((acc, b) => acc + b.buffer.length, 0)
+  const channelBuffer = audioCtx.createBuffer(
+    1,
+    totalBufferLength,
+    buffers[0].buffer.sampleRate
+  )
+
+  const channel = channelBuffer.getChannelData(0);
+  let bufferOffset = 0
+  const bufferOffsets = buffers
+    .map(b => {
+      const thisBufferOffset = bufferOffset
+      bufferOffset = bufferOffset + b.buffer.length
+      return thisBufferOffset
+    })
+
+  buffers.forEach((buffer, index) => {
+    channel.set(buffer.buffer.getChannelData(0), bufferOffsets[index]);
+  })
+  return channelBuffer;
+}
+
+
+async function addChannel (recording: Recording, channelIndex: number): Promise<AudioBufferSourceNode> {
+  const channelBuffers = await Promise.all(recording.trms.map(trm => getAudioBuffer(trm.channels[channelIndex].path)))
+
+  const channelSrc = audioCtx.createBufferSource()
+
+  const channelBuffer = concatenateBuffers(channelBuffers)
+  channelSrc.buffer = channelBuffer
   const gain = audioCtx.createGain()
-  src.connect(gain)
+  channelSrc.connect(gain)
 
   gain.connect(audioCtx.destination)
 
@@ -60,14 +120,19 @@ async function addAudio(audio: Audio): Promise<AudioBufferSourceNode> {
   })
 
   const label = document.createElement('label')
-  label.textContent = audio.name
+  label.textContent = channelIndex.toString()
   const container = document.createElement('div')
   container.appendChild(label)
   container.appendChild(input)
   document.body.appendChild(container)
 
-  return src
+  return channelSrc
 }
 
-Promise.all(audios.map(addAudio))
-.then(buffers => buffers.forEach(buffer => buffer.start()))
+Promise
+  .all(
+    Array(recording.numberOfChannels)
+      .fill(0)
+      .map((_, channelIndex) => addChannel(recording, channelIndex))
+  )
+  .then(channelSrcs => channelSrcs.forEach(channelSrc => channelSrc.start()))
